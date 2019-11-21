10a72dd8e9a4fc73bb9e74e5586460b99b3f24f1
AAN <antonioabunassar@gmail.com>
2019-11-21 13:03:08 +0200

    Answer part 3 dry

--- answers.py
+++ answers.py
@@ -25,14 +25,6 @@ part2_q2 = r"""
 The issue with this method is that we do not exploit all our dataset (we dedicate a part of it to be only use for test purpose) to train our model as much as we can.
 
 The k-folder method presents a good trade-off between these two possibilities.
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
 """
 
 # ==============
@@ -41,42 +33,22 @@ An equation: $e^{i\pi} -1 = 0$
 # Part 3 answers
 
 part3_q1 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+The value of $\Delta$ was arbitrary since the value of $\Delta$ doesn't matter as much as it matters that it exists.
+The goal of having $\Delta$ was to make sure there was some margin that distances the score of the correct class from the other classes. Increasing or decreasing $\Delta$ scales the weights matrix while preserving the ratio between the weights, which is what really matters to the convergence. Therefore, the choice of $\Delta$ can be an arbitrary positive number.
 """
 
 part3_q2 = r"""
-**Your answer:**
+Typically, each image in the visualization section corresponds to a digit. Every image would show us what, according to what the model learned, every digit should look like.
+That's why, the errors in classification are explained by the fact that if a digit is not clear or is rotated then
+the scores will be different and we will get the wrong classification.
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+This is similar to kNN in the sense that both models, at some point, use L2 distance between the image we want to predict and the training set. In kNN, we calculate the distance of the representative vectors and pick the nearest, while in this model we calculate the distance during training and predict by using the weights matrix.
 """
 
 part3_q3 = r"""
-**Your answer:**
+We believe that the learning rate is good, since the loss converges neatly to a very low value. On the other hand, it converged after a very small number of epochs, then it continued to decrease way slower, so the learning rate might have been a bit too high. Typically, we would prefer a steady convergence so that we don't risk stepping over the minimum. If we picked a lower learning rate, the convergence would be a bit slower. A higher learning rate would, as said, have the risk of stepping over the minimum.
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+The model is slightly overfitted to the training set. According to the accuracy graph, the training set accuracy becomes higher than the validation set accuracy, and it can be seen in the example images that a slight change in the number's rotation would imply a mislabeled image. But since the accuracy keeps improving along the epochs for both sets, the model is only slightly overfitted.
 """
 
 # ==============
